f5d50dcae9a15b1e7455c54391a3e5b420132b65
houyemlahmar <houyem.lahmar.1999@gmail.com>
2026-01-02 12:45:30 +0100

    add Config service & Discovery service

---
+++ backend/ms_eureka/src/main/java/com/example/demo/MsEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer  // ajouter l'API Eureka Server
+@SpringBootApplication
+public class MsEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaApplication.class, args);
+	}
+
+}
